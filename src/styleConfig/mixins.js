40a3f016aee49d0fd594d08e0b03de32d8a3b11b
Adam Wagner <aagnerjcu08@gmail.com>
2017-10-30 13:06:39 -0700

    Updating mixins

--- src/styleConfig/mixins.js
+++ src/styleConfig/mixins.js
@@ -1,16 +1,16 @@
-import {css} from "styled-components"; // https://github.com/donavon/styled-shortcuts
+import { css } from "styled-components"; // https://github.com/donavon/styled-shortcuts
 import { responsiveStyle } from "styled-system";
-import { without, curry } from 'ramda'
+import { without, curry } from "ramda";
 
 export const absPseudo = css`
   position: absolute;
   top: 0;
-  bottom:0;
-  left:0;
-  right:0;
-  content: '';
+  bottom: 0;
+  left: 0;
+  right: 0;
+  content: "";
   display: block;
-`
+`;
 
 export const clickableReset = css`
   cursor: pointer;
@@ -19,8 +19,7 @@ export const clickableReset = css`
   user-select: none;
   -webkit-appearance: none;
   -webkit-tap-highlight-color: transparent;
-
-`
+`;
 
 // TODO: uncouple from display value
 export const show = responsiveStyle({
@@ -31,7 +30,6 @@ export const show = responsiveStyle({
   cssProperty: "display"
 });
 
-
 /*props.border is an array of space-separated border directions
   corresponding to breakpoints. E.g.,
   props.border={['top left', 'top']} would result in
@@ -40,55 +38,49 @@ export const show = responsiveStyle({
 */
 
 // TODO: accept non-responsive values (just string, not array)
-export const borderHelper = ({border, theme}) => {
+export const borderHelper = ({ border:borderProp, theme }) => {
   let borderMaker = curry((target, bp, rule, val) => {
     target.push(`@media (min-width: ${bp}) {
                       border-${val}: ${rule};
-                    }`)
+                    }`);
   });
 
-  let cssRules = [] // our final product
-  let possibleDirs = ['top', 'right', 'bottom', 'left'];
+  let cssRules = []; // our final product
+  let possibleDirs = ["top", "right", "bottom", "left"];
 
   // make a copy and add a starting breakpoint of 0 that rebass assumes
   let breakpoints = Array.from(theme.breakpoints);
   breakpoints.unshift(0);
 
   let activeRule = `solid 1px ${theme.colors.gray1}`;
-  let inactiveRule = `none`
+  let inactiveRule = `none`;
 
+  if (borderProp) {
 
-  if (border) {
-
-    console.log('is string?',typeof border === 'string');
-
-    if (Array.isArray(border)) {
-      console.log('in is array');
-
-      border.forEach((str, idx) => {
-        let bp = `${breakpoints[idx]}em`
+    if (Array.isArray(borderProp)) {
+      borderProp.forEach((str, idx) => {
+        let bp = `${breakpoints[idx]}em`;
         let makeBorders = borderMaker(cssRules, bp);
 
         // Add a border for the directions
         // specified at each breakpoint
-        let activeDirs = str.split(' ').map(i => i.trim());
-        activeDirs.forEach(val => makeBorders(activeRule, val))
-
+        let activeDirs = str.split(" ").map(i => i.trim());
+        activeDirs.forEach(val => makeBorders(activeRule, val));
 
         // Set border to 'none' for directions
         // *not* specified at the given breakpoints
         // to cancel out rules that will apply from earlier breakpoints.
         let inactiveDirs = without(activeDirs, possibleDirs);
-        inactiveDirs.forEach(val => makeBorders(inactiveRule, val))
-      })
-  } else if (typeof border === 'string') { // props.border is string
-    console.log('in string section');
-    let bp = '0em';
-    let makeBorders = borderMaker(cssRules, bp)
-    let activeDirs = border.split(' ').map(i => i.trim());
-    activeDirs.forEach(val => makeBorders(activeRule, val))
+        inactiveDirs.forEach(val => makeBorders(inactiveRule, val));
+      });
+
+    } else if (typeof borderProp === "string") {
+      let bp = "0em";
+      let makeBorders = borderMaker(cssRules, bp);
+      let activeDirs = borderProp.split(" ").map(i => i.trim());
+      activeDirs.forEach(val => makeBorders(activeRule, val));
+    }
   }
-}
 
   return cssRules.join(`\n`);
-}
+};
